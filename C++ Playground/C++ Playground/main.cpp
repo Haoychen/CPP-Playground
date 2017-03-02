d46158e1250c9754a753c8b54696fd20d07cb976
Haoychen <haoyang.chen@petuum.com>
2017-03-02 17:54:27 -0500

    Add project

---
+++ C++ Playground/C++ Playground/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  C++ Playground
+//
+//  Created by Haoychen on 3/2/17.
+//  Copyright © 2017 Haoychen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int i = 1, j = 1, k = 1;
+    if (i < j < k) {
+        std::cout << "true"<< std::endl;
+    }
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
